fd24248efc4c3e3306ebef125a0f86e4ac6cb056
Hunter <hklemetson@gmail.com>
2022-03-21 14:40:08 -0600

    updatedddd

--- ssg.py
+++ ssg.py
@@ -1,8 +1,13 @@
 import typer
+import ssg.parsers
 from ssg.site import Site
 
 def main(source="content", dest="dist"):
-    config = {"source": source, "dest": dest}
+    config = {
+        "source": source,
+        "dest": dest,
+        "parsers": [ ssg.parsersResourceParser(), ],
+        }
 
     Site(**config).build()
 
